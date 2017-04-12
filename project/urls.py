77ad579cd8a96b43726ed327e9378ae201a8f7ac
Laura Trabas Clavero <Laura@MacBook-Pro-de-Laura-2.local>
2017-04-12 19:12:12 +0200

    Ejercicio

--- project/urls.py
+++ project/urls.py
@@ -5,6 +5,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^(\d+)\+(\d+)$', "calc.views.suma"),
+    url(r'^(\d+)\-(\d+)$', "calc.views.resta"),
+    url(r'^(\d+)\*(\d+)$', "calc.views.multi"),
+    url(r'^(\d+)\/(\d+)$', "calc.views.divi"),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'.*', "calc.views.fallo")
 )
