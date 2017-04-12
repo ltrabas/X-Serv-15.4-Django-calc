00d8ef17f49ce5b771426eee4f7fb0fb581e797a
Laura Trabas Clavero <Laura@MacBook-Pro-de-Laura-2.local>
2017-04-12 19:11:21 +0200

    Ejercicio

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def suma(request, num1, num2):
+    resultado = int(num1) + int(num2)
+    return HttpResponse("<b><h1>" + str(num1) + " + " +
+                        str(num2) + " = " + str(resultado)+ "</h1></b>")
+
+def resta(request, num1, num2):
+    resultado = int(num1) - int(num2)
+    return HttpResponse("<b><h1>" + str(num1) + " - " +
+                        str(num2) + " = " + str(resultado)+ "</h1></b>")
+
+def multi(request, num1, num2):
+    resultado = int(num1) * int(num2)
+    return HttpResponse("<b><h1>" + str(num1) + " * " +
+                        str(num2) + " = " + str(resultado)+ "</h1></b>")
+
+def divi(request, num1, num2):
+    try:
+        resultado = int(num1) / int(num2)
+    except ZeroDivisionError:
+          return HttpResponse("<b> ¡Division entre cero! </b>")
+
+    return HttpResponse("<b><h1>" + str(num1) + " / " +
+                        str(num2) + " = " + str(resultado)+ "</h1></b>")
+
+def fallo(request):
+	return HttpResponse("<b><h1>404 Not Found</h1>" +
+                       "Fallo al introducir los numeros</b>")
